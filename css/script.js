fd28162b95f245cce3ac229cc2525fde9254ec04
azizboujaada <116186539+aziz-art@users.noreply.github.com>
2022-10-28 10:53:23 +0200

    oliveir

---
+++ css/script.js
@@ -0,0 +1,4 @@
+function aziz(){
+    var a=<iframe width="802" height="451" src="https://www.youtube.com/embed/YWXslECbfg4" title="إنتاج الرحالي لزيت الزيتون الحرة مباشرة بمنطقة العطاوية الرقم 1 في المغرب  مرحباً بالجميع 🇲🇦⁩" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    document.getElementById
+}
